536435cc1d04f17a8ead25ed2b07dc116c5c72c4
Regi666 <Sebastian.Altmann@gmail.com>
2017-04-12 22:17:37 +0200

    delete address

--- src/main/java/de/alt/senseless/controller/AddressController.java
+++ src/main/java/de/alt/senseless/controller/AddressController.java
@@ -1,43 +1,57 @@
 package de.alt.senseless.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.alt.senseless.model.entities.Address;
 import de.alt.senseless.model.repos.AddressRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/address/{name}")
 public class AddressController {
 
-    private final AddressRepository addressRepository;
+	private final AddressRepository addressRepository;
 
-    @Autowired
-    AddressController(AddressRepository addressRepository) {
-        this.addressRepository = addressRepository;
-    }
+	@Autowired
+	AddressController(AddressRepository addressRepository) {
+		this.addressRepository = addressRepository;
+	}
 
-    @RequestMapping(method = RequestMethod.GET)
-    Address readAddress(@PathVariable String name) {
-        return this.addressRepository.findByName(name);
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	Address readAddress(@PathVariable String name) {
+		return this.addressRepository.findByName(name);
+	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	Address writeAddress(@PathVariable String name, @RequestParam String ip) {
 
-    @RequestMapping(method = RequestMethod.POST)
-    Address writeAddress(@PathVariable String name, @RequestParam String ip) {
+		Address address = addressRepository.findByName(name);
 
-        Address address = addressRepository.findByName(name);
+		if (address != null) {
+			address.setLastUpdate(new Date());
+			address.setIp(ip);
+		} else {
+			address = new Address(name, ip);
+		}
 
-        if (address != null) {
-            address.setLastUpdate(new Date());
-            address.setIp(ip);
-        } else {
-            address = new Address(name, ip);
-        }
+		return addressRepository.save(address);
+	}
 
-        return addressRepository.save(address);
-    }
+	@RequestMapping(method = RequestMethod.DELETE)
+	public void deleteAddress(@PathVariable String name) {
+
+		Address address = addressRepository.findByName(name);
+
+		if (address != null) {
+			addressRepository.delete(address.getId());
+		}
+
+	}
 
 }
